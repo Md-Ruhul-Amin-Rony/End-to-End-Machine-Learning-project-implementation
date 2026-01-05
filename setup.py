7ae4caef3b922793dc69786800bcaf6a72557615
Md-Ruhul-Amin-Rony <adiar1234@yahoo.com>
2026-01-05 09:08:41 +0100

    necessary requirements added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+setup(
+    name = "us_visa",
+    version="0.0.0",
+    author="Ruhul",
+    author_email="adiar1234@yahoo.com",
+    packages=find_packages()
+)
